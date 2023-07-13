8f86ffa5222597cd90bfb0fba8694118b3cbf77a
Landry DAVID <davidlandryjp@gmail.com>
2023-07-13 16:09:12 +0200

    :construction: add components, add login and signup form

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,9 +1,23 @@
+// import { Link } from 'react-router-dom';
 import './Login.scss';
 
 function Login() {
   return (
     <div className="Login">
-      <h1>Hello Login</h1>
+      <h3>Connexion</h3>
+      <form className="form">
+        <p>Email</p>
+        <input type="text" placeholder="email" />
+        <p>Mot de passe</p>
+        <input type="password" placeholder="Mot de passe" />
+      </form>
+      <a href="/account/resetPassword">Mot de passe oublié</a>
+      <button type="submit">Se connecter</button>
+      <p>Ou</p>
+      <p>
+        Vous n&apos;avez pas de compte{' '}
+        <a href="/signup">se créer un compte Jobmemo</a>
+      </p>
     </div>
   );
 }
--- src/components/SignUp/SignUp.tsx
+++ src/components/SignUp/SignUp.tsx
@@ -3,7 +3,20 @@ import './SignUp.scss';
 function SignUp() {
   return (
     <div className="SignUp">
-      <h1>Hello SignUp</h1>
+      <h3>Se créer un compte</h3>
+      <form className="form">
+        <p>Email</p>
+        <input type="text" placeholder="email" />
+        <p>Mot de passe</p>
+        <input type="password" placeholder="Mot de passe" />
+        <p>Confirmez votre mot de passe</p>
+        <input type="password" placeholder="Confirmez votre mot de passe" />
+        <p>Nom</p>
+        <input type="text" placeholder="Nom" />
+        <p>Prénom</p>
+        <input type="text" placeholder="Prénom" />
+      </form>
+      <button type="submit">S&apos;inscrire</button>
     </div>
   );
 }
