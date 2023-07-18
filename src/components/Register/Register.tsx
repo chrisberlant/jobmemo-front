68dd957fdd510c9d84e3e5b9a67b2f95ceea05e4
jo <58933188+jomacz@users.noreply.github.com>
2023-07-18 09:23:17 +0200

    add panel register (no validation)

--- src/components/Register/Register.tsx
+++ src/components/Register/Register.tsx
@@ -1,22 +1,148 @@
+// import { Link } from 'react-router-dom';
+import { gsap } from 'gsap';
+// import { useLayoutEffect, useRef } from 'react';
+import logo from '../../assets/images/logo.svg';
+// import check from '../../assets/icons/check.svg';
+// import error from '../../assets/icons/error.svg';
 import './Register.scss';
 
 function Register() {
+  // ANIMATION ////////////////////////////////////////////////////
+
+  // Animation des champs avec GSAP >>
+  // Animation lorsqu'il y'a une action sur le champ
+  const handleFocus = (e: React.FocusEvent<HTMLInputElement>) => {
+    const label = e.target.parentNode?.querySelector('label');
+    const line = e.target.parentNode?.querySelector('.line');
+    if (label && line) {
+      gsap.to(label, {
+        duration: 0.2,
+        y: -16,
+        color: '#4a65ff',
+      });
+      gsap.to(line, {
+        scaleX: 1,
+      });
+    }
+  };
+  // Animation lorsque l'on sort du champ
+  const handleBlur = (e: React.FocusEvent<HTMLInputElement>) => {
+    const label = e.target.parentNode?.querySelector('label');
+    const line = e.target.parentNode?.querySelector('.line');
+
+    if (label && line) {
+      if (e.target.value === '') {
+        gsap.to(label, {
+          duration: 0.1,
+          y: 0,
+          color: '#999',
+        });
+        gsap.to(line, {
+          scaleX: 0,
+        });
+      }
+    }
+  };
+  // FIN ANIMATION /////////////////////////////////////////////////////
+
+  // Register form submit
+  const handleSubmit = async (
+    e: React.ChangeEvent<HTMLFormElement>
+  ): Promise<void> => {
+    e.preventDefault();
+
+    const form = e.target;
+    const formData = new FormData(form);
+
+    // A virer (pour tester le contenu du formulaire dans la console)
+    const formJson = Object.fromEntries(formData.entries());
+    console.log(formJson);
+
+    // Appel à l'API
+    // try {
+    //   const fetchParams = {
+    //     method: 'POST',
+    //     body: formData,
+    //   };
+    //   const response = await fetch('http://localhost:3000/login', fetchParams);
+    //   const data = await response.json();
+    //   console.log(data);
+    // } catch (error) {
+    //   console.error(error);
+    // }
+  };
+
   return (
-    <div className="Register">
-      <h3>Se créer un compte</h3>
-      <form className="form">
-        <p>Email</p>
-        <input type="text" placeholder="email" />
-        <p>Mot de passe</p>
-        <input type="password" placeholder="Mot de passe" />
-        <p>Confirmez votre mot de passe</p>
-        <input type="password" placeholder="Confirmez votre mot de passe" />
-        <p>Nom</p>
-        <input type="text" placeholder="Nom" />
-        <p>Prénom</p>
-        <input type="text" placeholder="Prénom" />
-      </form>
-      <button type="submit">S&apos;inscrire</button>
+    <div className="box-wrap">
+      <div className="box-register">
+        <h2>Créer votre compte</h2>
+        <img className="logo" src={logo} alt="logo" />
+        <form method="post" onSubmit={handleSubmit}>
+          <div className="input-wrap">
+            <label htmlFor="firstname">Prénom : </label>
+            <input
+              onFocus={handleFocus}
+              onBlur={handleBlur}
+              type="text"
+              name="firstname"
+              id="firstname"
+              autoComplete="new-password"
+            />
+            <div className="line" />
+          </div>
+          <div className="input-wrap">
+            <label htmlFor="lastname">Nom : </label>
+            <input
+              onFocus={handleFocus}
+              onBlur={handleBlur}
+              type="text"
+              name="lastname"
+              id="lastname"
+              autoComplete="new-password"
+            />
+            <div className="line" />
+          </div>
+          <div className="input-wrap">
+            <label htmlFor="email">Email : </label>
+            <input
+              onFocus={handleFocus}
+              onBlur={handleBlur}
+              type="email"
+              name="email"
+              id="email"
+              autoComplete="new-password"
+            />
+            <div className="line" />
+          </div>
+          <div className="input-wrap">
+            <label htmlFor="password">Mot de passe : </label>
+            <input
+              onFocus={handleFocus}
+              onBlur={handleBlur}
+              type="password"
+              name="password"
+              id="password"
+              autoComplete="new-password"
+            />
+            <div className="line" />
+          </div>
+          <div className="input-wrap">
+            <label htmlFor="confirmPassword">Confirmez mot de passe : </label>
+            <input
+              onFocus={handleFocus}
+              onBlur={handleBlur}
+              type="password"
+              name="confirmPassword"
+              id="confirmPassword"
+              autoComplete="new-password"
+            />
+            <div className="line" />
+          </div>
+          <div className="input-wrap">
+            <input type="submit" defaultValue="Enregistrer" />
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
