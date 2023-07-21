72b731517ac1510d8e9bd6a15bae233a01c2f3a2
Landry DAVID <davidlandryjp@gmail.com>
2023-07-21 12:03:10 +0200

    :construction: full Navbar but without collapse animation

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -3,52 +3,89 @@ import { NavLink } from 'react-router-dom';
 import * as MaIcons from 'react-icons/md';
 import Calendar from 'react-calendar';
 import { useAppSelector } from '../hook/redux';
-import logo from '../../assets/images/logo.svg';
 import './Navbar.scss';
 
 function Navbar() {
   const user = useAppSelector((state) => state.user.firstName);
+  // Show/Hide Menu
   const [sideMenu, setSideMenu] = useState(false);
   const showsideMenu = () => {
     setSideMenu(!sideMenu);
     // console.log(sideMenu);
   };
+  //
+  // React Calendar
   const [date, setDate] = useState(new Date());
   const onchange = () => {
     setDate(date);
   };
+  //
 
   return (
     <div className="Navbar-container">
       <div className="Navbar">
-        <NavLink to="#" className="Menu-arrow">
+        <NavLink to="#" className="Menu-icons">
           <MaIcons.MdKeyboardDoubleArrowLeft onClick={showsideMenu} />
         </NavLink>
-        <img className="logo" src={logo} alt="logo" />
       </div>
       <nav className={sideMenu ? 'Nav-menu active' : 'Nav-menu'}>
+        <NavLink to="#" className="Menu-icons">
+          <MaIcons.MdKeyboardDoubleArrowRight />
+        </NavLink>
         <h3 className="Hello-text">Bienvenue {user} !</h3>
         <div className="Calendar-container">
           <Calendar onChange={onchange} value={date} />
         </div>
-        <ul className="Nav-menu_list">
-          <li className="Nav-menu_items">
-            <NavLink to="/dashboard">
+        <ul className="Nav-menu__list">
+          <li className="Nav-menu__items">
+            <NavLink
+              to="/dashboard"
+              style={({ isActive }) => ({
+                color: isActive ? '#a46eff' : '',
+                background: isActive ? '#fff' : '',
+                borderRadius: isActive ? '5px' : '',
+                padding: isActive ? '10px' : '',
+              })}
+            >
               <span>Mon tableau de bord</span>
             </NavLink>
           </li>
-          <li className="Nav-menu_items">
-            <NavLink to="/account">
+          <li className="Nav-menu__items">
+            <NavLink
+              to="/account"
+              style={({ isActive }) => ({
+                color: isActive ? '#a46eff' : '',
+                background: isActive ? '#fff' : '',
+                borderRadius: isActive ? '5px' : '',
+                padding: isActive ? '10px' : '',
+              })}
+            >
               <span>Mon compte</span>
             </NavLink>
           </li>
-          <li className="Nav-menu_items">
-            <NavLink to="/docs">
+          <li className="Nav-menu__items">
+            <NavLink
+              to="/docs"
+              style={({ isActive }) => ({
+                color: isActive ? '#a46eff' : '',
+                background: isActive ? '#fff' : '',
+                borderRadius: isActive ? '5px' : '',
+                padding: isActive ? '10px' : '',
+              })}
+            >
               <span>Mon drive</span>
             </NavLink>
           </li>
-          <li className="Nav-menu_items">
-            <NavLink to="/contacts">
+          <li className="Nav-menu__items">
+            <NavLink
+              to="/contacts"
+              style={({ isActive }) => ({
+                color: isActive ? '#a46eff' : '',
+                background: isActive ? '#fff' : '',
+                borderRadius: isActive ? '5px' : '',
+                padding: isActive ? '10px' : '',
+              })}
+            >
               <span>Mon réseau</span>
             </NavLink>
           </li>
