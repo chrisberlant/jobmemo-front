301b1f4146f17ee3a9b47fe65384754580ed0336
Landry DAVID <davidlandryjp@gmail.com>
2023-07-26 10:42:19 +0200

    :construction: Add CardForm content, add Quill text editor

--- src/components/CardForm/CardForm.tsx
+++ src/components/CardForm/CardForm.tsx
@@ -1,10 +1,16 @@
 import { useParams } from 'react-router-dom';
+import ReactQuill from 'react-quill';
+import { useState } from 'react';
 import logo from '../../assets/images/logo.svg';
 import './CardForm.scss';
+import 'react-quill/dist/quill.snow.css';
 
 function CardForm() {
   const { id } = useParams();
   console.log(id);
+
+  const [value, setValue] = useState('');
+
   return (
     <div className="box-wrap">
       <div className="box-cardform">
@@ -97,12 +103,7 @@ function CardForm() {
               </div>
               <div className="input-wrap">
                 <label htmlFor="phone">Téléphone : </label>
-                <input type="text" name="phone" id="phone" autoComplete="off" />
-                <div className="line" />
-              </div>
-              <div className="input-wrap">
-                <label htmlFor="enterprise">Ajouter depuis mon réseau : </label>
-                <input type="search" />
+                <input type="tel" name="phone" id="phone" autoComplete="off" />
                 <div className="line" />
               </div>
               <div className="input-wrap">
@@ -114,6 +115,18 @@ function CardForm() {
                   <option value="#">Entreprise 4</option>
                   <div className="line" />
                 </select>
+              </div>
+              <div className="input-wrap">
+                <label htmlFor="commentaries">Commentaires : </label>
+                <textarea
+                  name="commentaries"
+                  id="commentaries"
+                  autoComplete="off"
+                />
+                <div className="line" />
+              </div>
+              <div className="input-wrap">
+                <input type="submit" defaultValue="Enregistrer" />
               </div>
             </form>
           </div>
@@ -131,11 +144,66 @@ function CardForm() {
             <label htmlFor="offerUrl">Source / Lien de l&apos;annonce : </label>
             <input
               type="text"
-              name="enterpriseName"
-              id="enterpriseName"
+              name="offerUrl"
+              id="offerUrl"
               autoComplete="off"
             />
             <div className="line" />
+          </div>
+          <div className="input-wrap">
+            <label htmlFor="contractType">Type de contrat : </label>
+            <select className="contractType">
+              <option value="#">CDI</option>
+              <option value="#">CDD</option>
+              <option value="#">Alternance</option>
+              <option value="#">Intérim</option>
+              <div className="line" />
+            </select>
+          </div>
+          <div className="input-wrap">
+            <label htmlFor="commentaries">Descriptif du poste : </label>
+            <textarea
+              name="commentaries"
+              id="commentaries"
+              autoComplete="off"
+            />
+            <div className="line" />
+          </div>
+          <div className="input-wrap">
+            <label htmlFor="salary">Salaire : </label>
+            <input type="text" name="salary" id="salary" autoComplete="off" />
+            <div className="line" />
+          </div>
+          <div className="input-wrap">
+            <div className="box-editor">
+              <ReactQuill theme="snow" value={value} onChange={setValue} />
+            </div>
+          </div>
+          <div className="input-wrap">
+            <label htmlFor="docs">Documents associés : </label>
+            <select className="docs">
+              <option value="#">Document 01</option>
+              <option value="#">Document 02</option>
+              <option value="#">Document 03</option>
+              <option value="#">Document 04</option>
+              <div className="line" />
+            </select>
+          </div>
+          <h2>Ajouterun document depuis : </h2>
+          <div className="input-wrap">
+            <input type="submit" defaultValue="Drive" />
+          </div>
+          <div className="input-wrap">
+            <input type="submit" defaultValue="Ordinateur" />
+          </div>
+          <div className="input-wrap">
+            <input type="submit" defaultValue="Enregistrer" />
+          </div>
+          <div className="input-wrap">
+            <input type="submit" defaultValue="Annuler" />
+          </div>
+          <div className="input-wrap">
+            <span>Rating stars</span>
           </div>
         </form>
       </div>
