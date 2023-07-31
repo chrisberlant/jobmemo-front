a1774acdf85192e0d69bc67b73b72e2ced73e0e2
jo <58933188+jomacz@users.noreply.github.com>
2023-07-31 14:42:13 +0200

    Begin Responsive

--- src/components/Dashboard/Dashboard.tsx
+++ src/components/Dashboard/Dashboard.tsx
@@ -1,18 +1,43 @@
+import { useEffect, useMemo, useState } from 'react';
+import { useAppSelector } from '../../store/hook/redux';
+import useMediaQuery from '../../Utils/mediaQuery';
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
 import Kanban from '../Kanban/Kanban';
 import Navbar from '../Navbar/Navbar';
 import './Dashboard.scss';
-import { useAppSelector } from '../../store/hook/redux';
+
+// function useMediaQuery(query: string) {
+//   const mediaQuery = useMemo(() => window.matchMedia(query), [query]);
+//   const [match, setMatch] = useState(mediaQuery.matches);
+
+//   useEffect(() => {
+//     const change = () => setMatch(mediaQuery.matches);
+//     mediaQuery.addEventListener('change', change);
+//     return () => mediaQuery.removeEventListener('change', change);
+//   }, [mediaQuery]);
+
+//   return match;
+// }
+
+function useMediaQueries() {
+  const md = useMediaQuery('(max-width: 1250px)');
+
+  return { md };
+}
 
 function Dashboard() {
   const user = useAppSelector((state) => state.user.email);
+  const { md } = useMediaQueries();
 
   console.log(`Utilisateur connecté : ${user}`);
 
   return (
     <div className="dashboard">
-      <div className="navbar-container">
+      <div
+        className="navbar-container"
+        style={md ? { width: 0, display: 'none' } : { width: '100%' }}
+      >
         <Navbar />
       </div>
       {/* <div>{cardsItems}</div> */}
