e9aad8e0375f6ccc11f01ed3941b371e4fa90988
jo <58933188+jomacz@users.noreply.github.com>
2023-08-03 11:54:15 +0200

    CSS Contacts

--- src/components/Contacts/Contacts.tsx
+++ src/components/Contacts/Contacts.tsx
@@ -1,12 +1,21 @@
 import { useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { useAppDispatch, useAppSelector } from '../../store/hook/redux';
+// import useMediaQuery from '../../Utils/mediaQuery';
 import { getAllContacts } from '../../store/reducers/contacts';
 import Navbar from '../Navbar/Navbar';
 import Contact from './Contact/Contact';
 import './Contacts.scss';
 
+// function useMediaQueries() {
+//   const md = useMediaQuery('(max-width: 1250px)');
+
+//   return { md };
+// }
+
 function Contacts() {
+  // const { md } = useMediaQueries();
+
   console.log('Composant contacts');
   const contacts = useAppSelector((state) => state.contacts.list);
   const noContacts = useAppSelector((state) => state.contacts.isEmpty);
@@ -21,30 +30,41 @@ function Contacts() {
 
   return (
     <div className="Contacts">
-      {/* <Navbar /> */}
-      <h1>Vos contacts :</h1>
-      {!isLoading && (
-        <Link to="/createContact" className="add-contact-button">
-          <button type="button">Ajouter un nouveau contact</button>
-        </Link>
-      )}
-      <div className="contacts-container">
-        {isLoading && <span>Chargement en cours</span>}
+      <div
+        className="navbar-container"
+        // style={md ? { width: 0, display: 'none' } : { width: '100%' }}
+      >
+        <Navbar />
+      </div>
 
-        {!isLoading && noContacts ? (
-          <span>Aucun contact pour le moment</span>
-        ) : (
-          contacts.map((contact) => (
-            <Contact
-              key={contact.id}
-              id={contact.id}
-              firstName={contact.firstName}
-              lastName={contact.lastName}
-              enterprise={contact.enterprise}
-              occupation={contact.occupation}
-            />
-          ))
-        )}
+      <div className="contacts-container">
+        <div className="contacts-header">
+          <h1>Vos contacts :</h1>
+          {!isLoading && (
+            <Link to="/createContact" className="add-contact-button">
+              <button type="button">Ajouter un nouveau contact</button>
+            </Link>
+          )}
+        </div>
+
+        <div className="contacts-grid">
+          {isLoading && <span>Chargement en cours</span>}
+
+          {!isLoading && noContacts ? (
+            <span>Aucun contact pour le moment</span>
+          ) : (
+            contacts.map((contact) => (
+              <Contact
+                key={contact.id}
+                id={contact.id}
+                firstName={contact.firstName}
+                lastName={contact.lastName}
+                enterprise={contact.enterprise}
+                occupation={contact.occupation}
+              />
+            ))
+          )}
+        </div>
       </div>
     </div>
   );
