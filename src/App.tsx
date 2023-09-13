86beae21f2cacebc9ed86a9c11644b3880e88088
Chris Berlant <chris.berlant@gmail.com>
2023-09-13 12:43:09 +0200

    finished router configuration and main.tsx update

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,19 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Login from '../Login/Login';
-import Register from '../Register/Register';
-import ForgotPassword from '../ForgotPassword/ForgotPassword';
-import Account from '../Account/Account';
-import DeleteAccount from '../Account/DeleteAccount/DeleteAccount';
-import ChangePassword from '../Account/ChangePassword/ChangePassword';
-import Dashboard from '../Dashboard/Dashboard';
-import RecycleBin from '../RecycleBin/RecycleBin';
-import Contacts from '../Contacts/Contacts';
-import CardForm from '../CardForm/CardForm';
-import CardDetails from '../CardDetails/CardDetails';
-import ContactCreation from '../ContactCreation/ContactCreation';
-import ContactDetails from '../Contacts/ContactDetails/ContactDetails';
-import PageNotFound from '../PageNotFound/PageNotFound';
-import PrivateRoutes from '../../Utils/PrivateRoutes';
+import Login from './components/Login/Login';
+import Register from './components/Register/Register';
+import ForgotPassword from './components/ForgotPassword/ForgotPassword';
+import Account from './components/Account/Account';
+import DeleteAccount from './components/Account/DeleteAccount/DeleteAccount';
+import ChangePassword from './components/Account/ChangePassword/ChangePassword';
+import Dashboard from './components/Dashboard/Dashboard';
+import RecycleBin from './components/RecycleBin/RecycleBin';
+import Contacts from './components/Contacts/Contacts';
+import CardForm from './components/CardForm/CardForm';
+import CardDetails from './components/CardDetails/CardDetails';
+import ContactCreation from './components/ContactCreation/ContactCreation';
+import ContactDetails from './components/Contacts/ContactDetails/ContactDetails';
+import PageNotFound from './components/PageNotFound/PageNotFound';
+import PrivateRoutes from './Utils/PrivateRoutes';
 import './App.scss';
 
 function App() {
@@ -21,6 +21,7 @@ function App() {
     <div className="App">
       <Router>
         <Routes>
+          <Route element={<Login />} path="/" />
           <Route element={<Login />} path="/login" />
           <Route element={<Register />} path="/register" />
           <Route element={<ForgotPassword />} path="/forgotPassword" />
--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,10 @@
-// Set Up React Router (no Outlet yet)
-import React from 'react';
-import { RouterProvider } from 'react-router-dom';
-import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import store from './store';
-import App from './components/App/App';
+import App from './App';
 
-import './styles/index.scss';
-import { router } from './Router';
-
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+const container = document.getElementById('root') as HTMLElement;
+const root = createRoot(container);
 root.render(
   <Provider store={store}>
     <App />
